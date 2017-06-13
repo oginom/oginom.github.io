0d8e3a3433fd08b99ee450fbd68a654f0bd3f4fd
oginom <ma@ogino-no-MacBook-Pro.local>
2017-06-13 15:46:58 +0900

    import vegas

--- js/q1.js
+++ js/q1.js
@@ -2,6 +2,14 @@ $(function(){
   $('p').click(function(){
     $(this).text('clicked');
     $('p').css('background-color', '#ffff00');
+    $('body').vegas({
+      slides:[
+        {src:'res/p1.jpg'},
+        {src:'res/p2.jpg'},
+        {src:'res/p3.jpg'},
+        {src:'res/p4.jpg'}
+      ]
+    });
   });
 });
 
