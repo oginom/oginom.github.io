d426006f3bb9e63790a38f225fab7803caa07e42
oginom <ma@ogino-no-MacBook-Pro.local>
2017-06-14 12:59:34 +0900

    divide bgview and main view

--- js/q1.js
+++ js/q1.js
@@ -1,15 +1,18 @@
 $(function(){
   $('p').click(function(){
-    $(this).text('clicked');
+    //$(this).text('clicked');
     $('p').css('background-color', '#ffff00');
-    $('body').vegas({
-      slides:[
-        {src:'res/p1.jpg'},
-        {src:'res/p2.jpg'},
-        {src:'res/p3.jpg'},
-        {src:'res/p4.jpg'}
-      ]
-    });
+  });
+  $('#bg').vegas({
+    slides:[
+      {src:'res/p1.jpg'},
+      {src:'res/p2.jpg'},
+      {src:'res/p3.jpg'},
+      {src:'res/p4.jpg'}
+    ],
+    timer:false,
+    delay:8000,
+    overlay:false
   });
 });
 
